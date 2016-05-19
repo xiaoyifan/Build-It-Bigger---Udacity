e1bc80c2f4642d3e1d240ed7fce048b1008e81c4
xiaoyifan <xiaoyifan@uchicago.edu>
2016-05-18 22:49:35 -0700

    added interstitialAds in the app.

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointsAsyncTask.java
@@ -3,12 +3,16 @@ package com.udacity.gradle.builditbigger;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import com.example.yifan.myapplication.backend.jokeApi.JokeApi;
 import com.example.yifan.myapplication.backend.jokeApi.model.JokeBean;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.uchicago.yifan.jokelibrary.JokeDisplayActivity;
@@ -23,6 +27,7 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
     private static JokeApi jokeApi = null;
     private Context context;
     private ProgressBar mProgressBar;
+    private InterstitialAd mInterstitialAd;
 
     public EndpointsAsyncTask(ProgressBar progressBar, Context context){
         this.mProgressBar = progressBar;
@@ -60,14 +65,48 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
     }
 
     @Override
-    protected void onPostExecute(String result) {
+    protected void onPostExecute(final String result) {
         super.onPostExecute(result);
 
-        if (mProgressBar != null)
-        {
-            mProgressBar.setVisibility(View.GONE);
+        mInterstitialAd = new InterstitialAd(context);
+        mInterstitialAd.setAdUnitId("ca-app-pub-2733823621040973/6477787846");
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                super.onAdLoaded();
+                if (mProgressBar != null) {
+                    mProgressBar.setVisibility(View.GONE);
+                }
+
+                if (mInterstitialAd.isLoaded())
+                    mInterstitialAd.show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                super.onAdFailedToLoad(errorCode);
+                if (mProgressBar != null) {
+                    mProgressBar.setVisibility(View.GONE);
+                }
+                startJokeActivity(result);
+            }
+
+            @Override
+            public void onAdClosed() {
+                startJokeActivity(result);
+            }
+        });
+        AdRequest ar = new AdRequest
+                .Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+
+        try {
+            mInterstitialAd.loadAd(ar);
+        } catch(java.lang.NoClassDefFoundError error) {
+            error.printStackTrace();
+            Log.d(LOG_TAG, "ERROR LOADING AD");
         }
-        startJokeActivity(result);
     }
 
     private void startJokeActivity(String mResult) {
@@ -76,6 +115,4 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
-
-
 }
